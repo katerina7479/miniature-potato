8715ac6120e75025dcfd2b487079c59c0d9f2012
Katerina Hanson <katerina.hanson@gmail.com>
2018-01-14 13:01:12 -0800

    Add settings and password change

--- hello/urls.py
+++ hello/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+    url(r'^settings/$', hello.views.settings, name='settings'),
+    url(r'^settings/password/$', hello.views.password, name='password'),
     url(r'^home', hello.views.home, name='home'),
     url(r'^db', hello.views.db, name='db'),
+
 ]
