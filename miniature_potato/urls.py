d767fa98bb97448436fadf93fecccab0f4179b6d
Katerina Hanson <katerina.hanson@gmail.com>
2018-01-14 13:58:02 -0800

    Moving to api structure

--- miniature_potato/urls.py
+++ miniature_potato/urls.py
@@ -6,22 +6,19 @@ from rest_framework.authtoken import views as drf_views
 
 admin.autodiscover()
 
-import hello.views
+import miniature_potato.views as app_views
 
-# Examples:
-# url(r'^$', 'hello.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', app_views.index, name='index'),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
-    url(r'^settings/$', hello.views.settings, name='settings'),
-    url(r'^settings/password/$', hello.views.password, name='password'),
-    url(r'^home', hello.views.home, name='home'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^settings/$', app_views.settings, name='settings'),
+    url(r'^settings/password/$', app_views.password, name='password'),
+    url(r'^home', app_views.home, name='home'),
+    url(r'^db', app_views.db, name='db'),
 
 ]
--- miniature_potato/wsgi.py
+++ miniature_potato/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hello project.
+WSGI config for miniature_potato project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniature_potato.settings")
 
 from django.core.wsgi import get_wsgi_application
 
