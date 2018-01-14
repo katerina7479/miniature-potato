b7f406777c095f20998b0690deb147333ac2a9c8
Katerina Hanson <katerina.hanson@gmail.com>
2018-01-14 13:48:17 -0800

    Adding drf

--- hello/settings.py
+++ hello/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'rest_framework',
+    'rest_framework.authtoken',
     'hello'
 ]
 
--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from django.urls import path
-
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+
+from rest_framework.authtoken import views as drf_views
+
 admin.autodiscover()
 
 import hello.views
@@ -16,7 +17,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
     url(r'^settings/$', hello.views.settings, name='settings'),
     url(r'^settings/password/$', hello.views.password, name='password'),
     url(r'^home', hello.views.home, name='home'),
