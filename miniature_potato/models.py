4ce250d439ea1059f47a2e3c79bf66c180a2845f
Katerina Hanson <katerina.hanson@gmail.com>
2018-01-14 19:08:56 -0800

    Backend Todo model

--- miniature_potato/models.py
+++ miniature_potato/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+class Todo(models.Model):
+    text = models.TextField()
+    createdAt = models.DateTimeField('created_at', auto_now_add=True)
+    completedAt = models.DateTimeField('completed_at', null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
